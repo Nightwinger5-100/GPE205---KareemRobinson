8c727c15c12ce9494684529adf32119681d28a1f
Nightwinger5-100 <karrobin@uat.edu>
2024-04-28 21:27:24 -0500

    Music, Colored Ui
    I added menu and ingame music. Additionally, I colored to ui, so it's no longer just black and white.

--- Assets/Scripts/Buttons/NormalButtons/ButtonPressToGame.cs
+++ Assets/Scripts/Buttons/NormalButtons/ButtonPressToGame.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ButtonPressToGame : MonoBehaviour
 {
+    public AudioSource menuMusic;
+    public AudioSource ingameMusic;
     public void ChangeToGameplay ()
     {
-        if (GameManager.instance != null) {
+        if (GameManager.instance != null) 
+        {
             GameManager.instance.ActivateGameplay();
+            menuMusic.Stop();
+            ingameMusic.Play();
         }
     }
 }
--- Assets/Scripts/Buttons/NormalButtons/ButtonPressToTitle.cs
+++ Assets/Scripts/Buttons/NormalButtons/ButtonPressToTitle.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ButtonPressToTitle : MonoBehaviour
 {
+    public AudioSource musicPlayer;
+
     public void ChangeToTitleScreen ()
     {
-        if (GameManager.instance != null) {
+        if (GameManager.instance != null) 
+        {
             GameManager.instance.ActivateTitleScreen();
+            musicPlayer.Play();
         }
     }
 }
